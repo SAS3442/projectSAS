d5c3d6ebf4976172c3ca5d4b7f10458492289466
avinash84319 <avichinnu22@gmail.com>
2023-03-18 12:32:06 +0530

    got the tockens back from google avinash 3

--- server.js
+++ server.js
@@ -14,16 +14,46 @@ const oauth2Client= new google.auth.OAuth2(
     process.env.REDIRECT_URL
 );
 
-const url = oauth2Client.generateAuthUrl({
+const url = oauth2Client.generateAuthUrl({   //creating the auth url from the information in env file
     access_type: 'offline',
     scope:process.env.SCOPES
   });
 
+async function reqfunction(code){
+    const {tokens} = await oauth2Client.getToken(code)
+    oauth2Client.setCredentials(tokens);
+    const twenty=await 20;
+
+    await new Promise((resolve, reject) =>{
+        setTimeout(resolve,1000);
+    });
+
+    console.log(tokens);
+    return [tokens.id_token,tokens.access_token];             //geting the token from google auth
+}
+
+
 app.get("/",(req,res)=>{
-    res.redirect(url)
+    res.redirect(url) //using the auth url
 })
 
-app.get("/home",(req,res)=>{
+app.get("/home", async (req,res)=>{
+    //get the token from qs
+    const code=req.query.code.toString();
+    //get the id and access token with the code
+    const [id_token,access_tocken]= await reqfunction(code);
+    console.log([id_token,access_tocken])  //values are coming for now in terms of array change this to object and change name of the fucntion
+    //get users with tocken
+
+    //upsert the token
+
+    //create a new session
+
+    //create access & refresh token
+
+    //set cookies
+
+    //redirect back
     res.render("home")
 })
 
